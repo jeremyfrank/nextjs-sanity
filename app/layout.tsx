6271fc19875c4d583e8be1414c74b58e06b92385
Jeremy Frank <jeremyjfrank@gmail.com>
2024-02-28 19:57:18 -0500

    Enable draft mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { draftMode } from "next/headers";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
+import LiveVisualEditing from "@/components/LiveVisualEditing";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {draftMode().isEnabled && <LiveVisualEditing />}
+      </body>
     </html>
   );
 }
